2aa46b8464f12957a0aad113f7c03cd1163afb79
flexhamp <flexhamp@gmail.com>
2016-10-10 10:21:26 +0700

    Изменил мультимапу, теперь она ConcurrentHashMap  и значения кладутся в CopyOnWriteArrayList, а не в ArrayList

--- src/main/java/ru/sbt/homework/chat/datastorage/MultipleMapImpl.java
+++ src/main/java/ru/sbt/homework/chat/datastorage/MultipleMapImpl.java
@@ -1,22 +1,23 @@
 package ru.sbt.homework.chat.datastorage;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 /**
  * Created by Admin on 08.10.2016.
  */
-public class MultipleMapImpl<K, V> implements MultipleMap<K, V>{
+public class MultipleMapImpl<K, V> implements MultipleMap<K, V> {
 
-    private Map<K, List<V>> Vs = new HashMap<>();
+    private Map<K, List<V>> Vs = new ConcurrentHashMap<>();
 
     @Override
     public void put(K key, V value) {
         List<V> Vs = get(key);
         if (Vs == null) {
-            Vs = new ArrayList<>();
+            Vs = new CopyOnWriteArrayList<V>();
             this.Vs.put(key, Vs);
         }
         Vs.add(value);
@@ -29,7 +30,7 @@ public class MultipleMapImpl<K, V> implements MultipleMap<K, V>{
 
     @Override
     public List<V> getAll() {
-        List<V> result = new ArrayList<V>();
+        List<V> result = new CopyOnWriteArrayList<V>();
         for (List<V> item : Vs.values()) {
             result.addAll(item);
         }
